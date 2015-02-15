6f94a921d0aaa3e1976a22cfadea050e49d25a00
Padmarajsinh <padmarajraijada@gmail.com>
2015-02-16 00:53:24 +0530

    changes done in IdentityCounter model to handle Multi-tenant senario

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 var mongoose = require('mongoose'),
     extend = require('extend'),
     counterSchema,
-    IdentityCounter;
+    IdentityCounter,
+Schema = mongoose.Schema;;
 
 // Initialize plugin by creating counter collection in database.
 exports.initialize = function (connection) {
@@ -15,7 +16,11 @@ exports.initialize = function (connection) {
                 model: { type: String, require: true },
                 field: { type: String, require: true },
                 count: { type: Number, default: 0 },
-                collectionName:{type:String,required:true}
+                collectionName:{type:String,required:true},
+                tenantId:{type:String, required:true},
+                prefix:{type:String},
+                size:{type:String}
+
             });
 
             // Create a unique index using the "field" and "model" fields.
@@ -42,7 +47,8 @@ exports.plugin = function (schema, options) {
             model: null, // The model to configure the plugin for.
             field: '_id', // The field the plugin should track.
             startAt: 0, // The number the count should start at.
-            incrementBy: 1 // The number by which to increment the count each time.
+            incrementBy: 1, // The number by which to increment the count each time.
+            size:"10"
         },
         fields = {}, // A hash of fields to add properties to in Mongoose.
         ready = true; // True if the counter collection has been updated and the document is ready to be saved.
@@ -67,52 +73,52 @@ exports.plugin = function (schema, options) {
     schema.add(fields);
 
     // Find the counter for this model and the relevant field.
-   /*
-    IdentityCounter.findOne(
-        { model: settings.model, field: settings.field },
-        function (err, counter) {
-            if (!counter) {
-                // If no counter exists then create one and save it.
-                counter = new IdentityCounter({ model: settings.model, field: settings.field, count: settings.startAt - settings.incrementBy });
-                counter.save(function () {
-                    ready = true;
-                });
-            }
-            else
-                ready = true;
-        }
-    );
+    /*
+     IdentityCounter.findOne(
+     { model: settings.model, field: settings.field },
+     function (err, counter) {
+     if (!counter) {
+     // If no counter exists then create one and save it.
+     counter = new IdentityCounter({ model: settings.model, field: settings.field, count: settings.startAt - settings.incrementBy });
+     counter.save(function () {
+     ready = true;
+     });
+     }
+     else
+     ready = true;
+     }
+     );
 
-    // Declare a function to get the next counter for the model/schema.
-    var nextCount = function (callback) {
-        IdentityCounter.findOne({
-            model: settings.model,
-            field: settings.field
-        }, function (err, counter) {
-            if (err) return callback(err);
-            callback(null, counter === null ? settings.startAt : counter.count + settings.incrementBy);
-        });
-    };
-    // Add nextCount as both a method on documents and a static on the schema for convenience.
-    schema.method('nextCount', nextCount);
-    schema.static('nextCount', nextCount);
+     // Declare a function to get the next counter for the model/schema.
+     var nextCount = function (callback) {
+     IdentityCounter.findOne({
+     model: settings.model,
+     field: settings.field
+     }, function (err, counter) {
+     if (err) return callback(err);
+     callback(null, counter === null ? settings.startAt : counter.count + settings.incrementBy);
+     });
+     };
+     // Add nextCount as both a method on documents and a static on the schema for convenience.
+     schema.method('nextCount', nextCount);
+     schema.static('nextCount', nextCount);
 
-    // Declare a function to reset counter at the start value - increment value.
-    var resetCount = function (callback) {
-        IdentityCounter.findOneAndUpdate(
-            { model: settings.model, field: settings.field },
-            { count: settings.startAt - settings.incrementBy },
-            { new: true }, // new: true specifies that the callback should get the updated counter.
-            function (err) {
-                if (err) return callback(err);
-                callback(null, settings.startAt);
-            }
-        );
-    };
-    // Add resetCount as both a method on documents and a static on the schema for convenience.
-    schema.method('resetCount', resetCount);
-    schema.static('resetCount', resetCount);
-    */
+     // Declare a function to reset counter at the start value - increment value.
+     var resetCount = function (callback) {
+     IdentityCounter.findOneAndUpdate(
+     { model: settings.model, field: settings.field },
+     { count: settings.startAt - settings.incrementBy },
+     { new: true }, // new: true specifies that the callback should get the updated counter.
+     function (err) {
+     if (err) return callback(err);
+     callback(null, settings.startAt);
+     }
+     );
+     };
+     // Add resetCount as both a method on documents and a static on the schema for convenience.
+     schema.method('resetCount', resetCount);
+     schema.static('resetCount', resetCount);
+     */
     // Every time documents in this schema are saved, run this logic.
     schema.pre('save', function (next) {
         // Get reference to the document being saved.
@@ -142,18 +148,26 @@ exports.plugin = function (schema, options) {
                         function (err, updatedIdentityCounter) {
                             if (err) return next(err);
                             // If there are no errors then go ahead and set the document's field to the current count.
-                           var prefix="CUS00000";
+                            //Padmaraj--To Identify Collection name and create customized ID field for all models
+                            //Creating funtion will return Id as per the inputs.
+
                             if(updatedIdentityCounter) {
 
-                                doc[settings.field] = prefix+updatedIdentityCounter.count;
-                                //doc._id=updatedIdentityCounter.count;
-                                var a;
+
+
+                                var str = new Array(parseInt(updatedIdentityCounter.size) + 1).join( "0" );
+                                //console.log(str);
+                                var seqPart2= (str + updatedIdentityCounter.count).slice(-parseInt(updatedIdentityCounter.size));
+                                //console.log(seqPart2);
+                                doc[settings.field] = updatedIdentityCounter.prefix + seqPart2;;
+
                             }
                             else {
                                 doc[settings.field] =doc._id=prefix+0;
                             }
                             // Continue with default document save functionality.
                             next();
+
                         }
                     );
                 }
@@ -168,4 +182,61 @@ exports.plugin = function (schema, options) {
         else
             next();
     });
+};
+
+
+var idConfigSchema = new Schema({
+    colName:{
+        type:String
+    },
+    modName:{
+        type:String
+    },
+    tenantId:{
+        type:String
+    },
+    prefix:{
+        type:String
+    },
+    size:{
+        type:String
+    }
+});
+
+var IdConfig=mongoose.model('idconfig', idConfigSchema);
+
+
+
+function nextCounter(doc,counter){
+
+   var IdConfig = mongoose.model('idconfig');
+   IdConfig.findOne(
+   {
+       colName:doc.__proto__.collection.name,
+       tenantId:doc.__proto__.schema.$tenantId,
+       modName:doc.__proto__.schema.discriminatorMapping.value
+
+
+   },function(err,res){
+
+
+
+           if (err){
+               console.log(err);
+           }
+           else{
+               console.log(res);
+
+               var str = new Array(parseInt(res.size) + 1).join( "0" );
+               console.log(str);
+               var seqPart2= (str + counter.count).slice(-parseInt(res.size));
+               console.log(seqPart2);
+               return res.prefix + seqPart2;
+               //console.log(seq);
+               //return seq;
+           }
+
+   });
+
+
 };
